bf54ddf6da0433c010b7f2cad91323acc73669cd
CrazyHub <78809386+CrazyHub123@users.noreply.github.com>
2024-07-30 17:12:59 +0300

    Create Test.lua

---
+++ Test.lua
@@ -0,0 +1,5 @@
+if getgenv().Key ~= nil then
+  if getgenv().Key == loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusKey/main/Key.txt"))() then
+    print("z")
+  end
+end
